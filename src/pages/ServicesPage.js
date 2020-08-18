99358f037926cf01004f7eb13125fdd3d94cc047
DreamsOfImran <ibasha66@gmail.com>
2020-08-18 09:32:11 +0530

    Base react app without router

---
+++ src/pages/ServicesPage.js
@@ -0,0 +1,357 @@
+import React from "react";
+
+const ServicesPage = (props) => {
+  return (
+    <React.Fragment>
+      <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+          <div class="preloader-inner position-relative">
+            <div class="preloader-circle"></div>
+            <div class="preloader-img pere-text">
+              <img src="assets/img/logo/loder.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <header>
+        <div class="header-area">
+          <div class="main-header  header-sticky">
+            <div class="container-fluid">
+              <div class="row align-items-center">
+                <div class="col-xl-2 col-lg-2 col-md-1">
+                  <div class="logo">
+                    <a href="index.html">
+                      <img src="assets/img/logo/logo.png" alt="" />
+                    </a>
+                  </div>
+                </div>
+                <div class="col-xl-10 col-lg-10 col-md-10">
+                  <div class="menu-main d-flex align-items-center justify-content-end">
+                    <div class="main-menu f-right d-none d-lg-block">
+                      <nav>
+                        <ul id="navigation">
+                          <li>
+                            <a href="index.html">Home</a>
+                          </li>
+                          <li>
+                            <a href="about.html">About</a>
+                          </li>
+                          <li>
+                            <a href="services.html">Services</a>
+                          </li>
+                          <li>
+                            <a href="portfolio.html">Portfolio</a>
+                          </li>
+                          <li>
+                            <a href="#">Page</a>
+                            <ul class="submenu">
+                              <li>
+                                <a href="blog.html">Blog</a>
+                              </li>
+                              <li>
+                                <a href="blog_details.html">Blog Details</a>
+                              </li>
+                              <li>
+                                <a href="elements.html">Element</a>
+                              </li>
+                              <li>
+                                <a href="portfolio_details.html">
+                                  Portfolio Details
+                                </a>
+                              </li>
+                            </ul>
+                          </li>
+                          <li>
+                            <a href="contact.html">Contact</a>
+                          </li>
+                        </ul>
+                      </nav>
+                    </div>
+                    <div class="header-right-btn f-right d-none d-xl-block ml-20">
+                      <a href="#" class="btn header-btn">
+                        Get Free Consultent
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="mobile_menu d-block d-lg-none"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div class="slider-area ">
+          <div class="slider-height2 d-flex align-items-center">
+            <div class="container">
+              <div class="row">
+                <div class="col-xl-12">
+                  <div class="hero-cap">
+                    <h2>My Services</h2>
+                    <nav aria-label="breadcrumb ">
+                      <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                          <a href="index.html">Home</a>
+                        </li>
+                        <li class="breadcrumb-item">
+                          <a href="#">My Services</a>
+                        </li>
+                      </ol>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <section class="categories-area categories-area2  section-padding30">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center mb-50">
+                  <div class="cat-icon">
+                    <span class="flaticon-pen"></span>
+                  </div>
+                  <div class="cat-cap">
+                    <h5>
+                      <a href="services.html">UI/UX Design</a>
+                    </h5>
+                    <p>
+                      Free resource that will help nderstand thecv designc
+                      process and improve theroi nderstand the design process
+                      andisei impro are of vquality.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center mb-50">
+                  <div class="cat-icon">
+                    <span class="flaticon-speaker"></span>
+                  </div>
+                  <div class="cat-cap">
+                    <h5>
+                      <a href="services.html">Digital Marketing</a>
+                    </h5>
+                    <p>
+                      Free resource that will help nderstand thecv designc
+                      process and improve theroi nderstand the design process
+                      andisei impro are of vquality.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center mb-50">
+                  <div class="cat-icon">
+                    <span class="flaticon-portfolio"></span>
+                  </div>
+                  <div class="cat-cap">
+                    <h5>
+                      <a href="services.html">Website Design</a>
+                    </h5>
+                    <p>
+                      Free resource that will help nderstand thecv designc
+                      process and improve theroi nderstand the design process
+                      andisei impro are of vquality.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center mb-50">
+                  <div class="cat-icon">
+                    <span class="flaticon-portfolio"></span>
+                  </div>
+                  <div class="cat-cap">
+                    <h5>
+                      <a href="services.html">Website Design</a>
+                    </h5>
+                    <p>
+                      Free resource that will help nderstand thecv designc
+                      process and improve theroi nderstand the design process
+                      andisei impro are of vquality.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center mb-50">
+                  <div class="cat-icon">
+                    <span class="flaticon-speaker"></span>
+                  </div>
+                  <div class="cat-cap">
+                    <h5>
+                      <a href="services.html">Digital Marketing</a>
+                    </h5>
+                    <p>
+                      Free resource that will help nderstand thecv designc
+                      process and improve theroi nderstand the design process
+                      andisei impro are of vquality.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center mb-50">
+                  <div class="cat-icon">
+                    <span class="flaticon-pen"></span>
+                  </div>
+                  <div class="cat-cap">
+                    <h5>
+                      <a href="services.html">UI/UX Design</a>
+                    </h5>
+                    <p>
+                      Free resource that will help nderstand thecv designc
+                      process and improve theroi nderstand the design process
+                      andisei impro are of vquality.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="wantToWork-area w-padding2">
+          <div class="container">
+            <div class="row align-items-center justify-content-between">
+              <div class="col-xl-6 col-lg-8 col-md-8">
+                <div class="wantToWork-caption wantToWork-caption2">
+                  <h2>Dont worry for contact i`m available</h2>
+                </div>
+              </div>
+              <div class="col-xl-2 col-lg-2 col-md-3">
+                <a href="#" class="btn btn-black f-right">
+                  Contact Me Now
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div class="brand-area section-padding30">
+          <div class="container">
+            <div class="brand-active brand-border pt-50 pb-40">
+              <div class="single-brand">
+                <img src="assets/img/gallery/brand1.png" alt="" />
+              </div>
+              <div class="single-brand">
+                <img src="assets/img/gallery/brand2.png" alt="" />
+              </div>
+              <div class="single-brand">
+                <img src="assets/img/gallery/brand3.png" alt="" />
+              </div>
+              <div class="single-brand">
+                <img src="assets/img/gallery/brand4.png" alt="" />
+              </div>
+              <div class="single-brand">
+                <img src="assets/img/gallery/brand2.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="contact-info-area w-padding2"
+          data-background="assets/img/gallery/section_bg04.jpg"
+        >
+          <div class="container">
+            <div class="row justify-content-between">
+              <div class="col-xl-5 col-lg-7">
+                <div class="contact-caption mb-50">
+                  <h3>If Not Now, When? Let’s Work Together!</h3>
+                  <p>
+                    Consectetur adipiscing elit, sed do eiusmod tempor ididunt
+                    ut labore et dolore magna aliqua. Quis ipsum suspendisces
+                    gravida. Risus commodo viverra.
+                  </p>
+                  <img src="assets/img/gallery/sin.png" alt="" />
+                </div>
+              </div>
+              <div class="col-xl-5 col-lg-5">
+                <form action="#" class="contact-wrapper">
+                  <input type="text" name="name" placeholder="Full Name" />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email Address"
+                  />
+                  <textarea
+                    name="message"
+                    id="message"
+                    placeholder="Your Message"
+                  ></textarea>
+                  <button class="submit-btn2" type="submit">
+                    Send Message
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <div class="footer-area">
+          <div class="container">
+            <div class="footer-top footer-padding">
+              <div class="row justify-content-center">
+                <div class="col-lg-6">
+                  <div class="footer-top-cap text-center">
+                    <img src="assets/img/logo/logo2_footer.png" alt="" />
+                    <span>
+                      <a href="#">hireme@portfolio.com</a>
+                    </span>
+                    <p>221B Baker Street, Post office Box 353 </p>
+                    <p>Park Road, USA - 215431</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="footer-bottom">
+              <div class="row d-flex justify-content-between align-items-center">
+                <div class="col-xl-9 col-lg-8">
+                  <div class="footer-copy-right">
+                    <p>
+                      Copyright &copy;
+                      <script>document.write(new Date().getFullYear());</script>{" "}
+                      All rights reserved | This template is made with{" "}
+                      <i class="fa fa-heart" aria-hidden="true"></i> by{" "}
+                      <a href="https://colorlib.com" target="_blank">
+                        Colorlib
+                      </a>
+                    </p>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-4">
+                  <div class="footer-social f-right">
+                    <a>Stay Connected</a>
+                    <a href="#">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="#">
+                      <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#">
+                      <i class="fas fa-globe"></i>
+                    </a>
+                    <a href="#">
+                      <i class="fab fa-instagram"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div id="back-top">
+        <a title="Go to Top" href="#">
+          {" "}
+          <i class="fas fa-level-up-alt"></i>
+        </a>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default ServicesPage;
