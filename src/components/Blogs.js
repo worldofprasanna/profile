e9855b85d276f13c975c5ef93c84736e934ac303
DreamsOfImran <ibasha66@gmail.com>
2020-08-23 18:26:05 +0530

    Showcasing blogs from Medium

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,6 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import MediumCard from "./MediumCard";
+import { Col } from "shards-react";
 
 const Blogs = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    arrows: true,
+    slidesToShow: 3,
+    prevArrow: (
+      <button type="button" className="slick-prev">
+        <i className="ti-angle-left"></i>
+      </button>
+    ),
+    nextArrow: (
+      <button type="button" className="slick-next">
+        <i className="ti-angle-right"></i>
+      </button>
+    ),
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const [posts, setPosts] = useState([]);
+  const [avatar, setAvatar] = useState("");
+  const [profilelink, setProfilelink] = useState("");
+
+  const mediumURL =
+    "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@worldofprasanna";
+
+  useEffect(() => {
+    fetch(mediumURL)
+      .then((res) => res.json())
+      .then((data) => {
+        setAvatar(data.feed.image);
+        setProfilelink(data.feed.link);
+        const res = data.items;
+        const fetchedPosts = res.filter((item) => item.categories.length > 0);
+        setPosts(fetchedPosts);
+      });
+  });
+
   return (
     <section className="services-area  services-padding">
       <div className="container">
@@ -14,7 +88,7 @@ const Blogs = () => {
             </div>
             <div className="col-lg-6">
               <div className="properties__button">
-                {/* <!--Nav Button  -->                                             */}
+                {/* <!--Nav Button  -->*/}
                 <nav>
                   <div className="nav nav-tabs" id="nav-tab" role="tablist">
                     <a
@@ -27,7 +101,7 @@ const Blogs = () => {
                       aria-selected="false"
                     >
                       {" "}
-                      All
+                      Blogs
                     </a>
                     <a
                       className="nav-item nav-link"
@@ -39,40 +113,7 @@ const Blogs = () => {
                       aria-selected="false"
                     >
                       {" "}
-                      Branding
-                    </a>
-                    <a
-                      className="nav-item nav-link"
-                      id="nav-contact-tab"
-                      data-toggle="tab"
-                      href="#nav-contact"
-                      role="tab"
-                      aria-controls="nav-contact"
-                      aria-selected="false"
-                    >
-                      Logo
-                    </a>
-                    <a
-                      className="nav-item nav-link"
-                      id="nav-last-tab"
-                      data-toggle="tab"
-                      href="#nav-last"
-                      role="tab"
-                      aria-controls="nav-contact"
-                      aria-selected="false"
-                    >
-                      UI/UX{" "}
-                    </a>
-                    <a
-                      className="nav-item nav-link"
-                      id="nav-technology"
-                      data-toggle="tab"
-                      href="#nav-techno"
-                      role="tab"
-                      aria-controls="nav-contact"
-                      aria-selected="false"
-                    >
-                      Web Design
+                      Github
                     </a>
                   </div>
                 </nav>
@@ -81,302 +122,25 @@ const Blogs = () => {
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-12">
-            {/* <!-- Nav Card --> */}
-            <div className="tab-content active" id="nav-tabContent">
-              {/* <!-- card ONE --> */}
-              <div
-                className="tab-pane fade active show"
-                id="nav-home"
-                role="tabpanel"
-                aria-labelledby="nav-home-tab"
-              >
-                <div className="services-caption">
-                  <div className="row">
-                    <div className="col-lg-8 col-md-8 col-sm-8">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services1.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      <div className="row">
-                        <div className="col-lg-12">
-                          {/* <!-- Single --> */}
-                          <div className="single-services mb-30">
-                            <img
-                              src="assets/img/service/services2.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                        <div className="col-lg-12">
-                          {/* <!-- Single --> */}
-                          <div className="single-services mb-30">
-                            <img
-                              src="assets/img/service/services3.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services4.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services5.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services6.png" alt="" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* <!-- Card TWO --> */}
-              <div
-                className="tab-pane fade"
-                id="nav-profile"
-                role="tabpanel"
-                aria-labelledby="nav-profile-tab"
-              >
-                <div className="services-caption">
-                  <div className="row">
-                    <div className="col-lg-8 col-md-8 col-sm-8">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services6.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      <div className="row">
-                        <div className="col-lg-12">
-                          {/* <!-- Single --> */}
-                          <div className="single-services mb-30">
-                            <img
-                              src="assets/img/service/services2.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                        <div className="col-lg-12">
-                          {/* <!-- Single --> */}
-                          <div className="single-services mb-30">
-                            <img
-                              src="assets/img/service/services3.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services4.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services5.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services1.png" alt="" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* <!-- Card THREE --> */}
-              <div
-                className="tab-pane fade"
-                id="nav-contact"
-                role="tabpanel"
-                aria-labelledby="nav-contact-tab"
-              >
-                <div className="services-caption">
-                  <div className="row">
-                    <div className="col-lg-8 col-md-8 col-sm-8">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services2.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      <div className="row">
-                        <div className="col-lg-12">
-                          {/* <!-- Single --> */}
-                          <div className="single-services mb-30">
-                            <img
-                              src="assets/img/service/services1.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                        <div className="col-lg-12">
-                          {/* <!-- Single --> */}
-                          <div className="single-services mb-30">
-                            <img
-                              src="assets/img/service/services3.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services4.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services5.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services6.png" alt="" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* <!-- card FUR --> */}
-              <div
-                className="tab-pane fade"
-                id="nav-last"
-                role="tabpanel"
-                aria-labelledby="nav-last-tab"
-              >
-                <div className="services-caption">
-                  <div className="row">
-                    <div className="col-lg-8 col-md-8 col-sm-8">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services1.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      <div className="row">
-                        <div className="col-lg-12">
-                          {/* <!-- Single --> */}
-                          <div className="single-services mb-30">
-                            <img
-                              src="assets/img/service/services2.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                        <div className="col-lg-12">
-                          {/* <!-- Single --> */}
-                          <div className="single-services mb-30">
-                            <img
-                              src="assets/img/service/services3.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services4.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services5.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services6.png" alt="" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* <!-- card FIVE --> */}
-              <div
-                className="tab-pane fade"
-                id="nav-techno"
-                role="tabpanel"
-                aria-labelledby="nav-technology"
-              >
-                <div className="services-caption">
-                  <div className="row">
-                    <div className="col-lg-8 col-md-8 col-sm-8">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services3.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      <div className="row">
-                        <div className="col-lg-12">
-                          {/* <!-- Single --> */}
-                          <div className="single-services mb-30">
-                            <img
-                              src="assets/img/service/services2.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                        <div className="col-lg-12">
-                          {/* <!-- Single --> */}
-                          <div className="single-services mb-30">
-                            <img
-                              src="assets/img/service/services1.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services4.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services5.png" alt="" />
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                      {/* <!-- Single --> */}
-                      <div className="single-services mb-30">
-                        <img src="assets/img/service/services6.png" alt="" />
-                      </div>
-                    </div>
-                  </div>
+        <div className="latest-blog-area">
+          <div className="container">
+            <div className="custom-row">
+              <div className="blog-active">
+                <div className="blog__slider">
+                  <Slider {...settings}>
+                    {posts.map((item) => (
+                      <Col key={item.title} className="mb-4">
+                        <MediumCard
+                          {...item}
+                          avatar={avatar}
+                          profilelink={profilelink}
+                        />
+                      </Col>
+                    ))}
+                  </Slider>
                 </div>
               </div>
             </div>
-            {/* <!-- End Nav Card --> */}
           </div>
         </div>
       </div>
