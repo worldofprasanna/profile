ac3ecd504a4f2352221f22a7b66260c535720179
DreamsOfImran <ibasha66@gmail.com>
2020-09-11 10:19:37 +0530

    Fixing services incorrect url

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const Services = () => {
   return (
@@ -20,10 +21,12 @@ const Services = () => {
               </div>
               <div className="cat-cap">
                 <h5>
-                  <a href="services.html">Devops</a>
+                  <Link style={{ cursor: "pointer" }}>Devops</Link>
                 </h5>
                 <p>
-                  Proficient in setting up CI/CD using variety of tools (Jenkins, CircleCI, Gitlab CI), Configuration Management tools (Ansible), Containers & Orchestration tools (Kubernetes)
+                  Proficient in setting up CI/CD using variety of tools
+                  (Jenkins, CircleCI, Gitlab CI), Configuration Management tools
+                  (Ansible), Containers & Orchestration tools (Kubernetes)
                 </p>
               </div>
             </div>
@@ -35,10 +38,15 @@ const Services = () => {
               </div>
               <div className="cat-cap">
                 <h5>
-                  <a href="services.html">Cloud / Infrastructure</a>
+                  <Link style={{ cursor: "pointer" }}>
+                    Cloud / Infrastructure
+                  </Link>
                 </h5>
                 <p>
-                Highly skilled in working with cloud environment (AWS, GCP, Digital Ocean, Heroku (Paas)). Extensively used Datadog, Prometheus, Cloudwatch to monitor various components of the Infrastructure
+                  Highly skilled in working with cloud environment (AWS, GCP,
+                  Digital Ocean, Heroku (Paas)). Extensively used Datadog,
+                  Prometheus, Cloudwatch to monitor various components of the
+                  Infrastructure
                 </p>
               </div>
             </div>
@@ -50,10 +58,12 @@ const Services = () => {
               </div>
               <div className="cat-cap">
                 <h5>
-                  <a href="services.html">Backend Developer</a>
+                  <Link style={{ cursor: "pointer" }}>Backend Developer</Link>
                 </h5>
                 <p>
-                  Comfortable with multiple tech stacks (Ruby on Rails, Scala) and has good experience in working with Distributed Systems & Streaming Data Analytics (Kafka, Cassandra, Flink, Spark)
+                  Comfortable with multiple tech stacks (Ruby on Rails, Scala)
+                  and has good experience in working with Distributed Systems &
+                  Streaming Data Analytics (Kafka, Cassandra, Flink, Spark)
                 </p>
               </div>
             </div>
